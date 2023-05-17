8f10ab36a28f780395d0345ba117edb059df5a22
Pavansomisetty32 <110320361+Pavansomisetty32@users.noreply.github.com>
2023-05-17 07:55:29 +0530

    AccountCreation.java

--- src/AccountCreation.java
+++ src/AccountCreation.java
@@ -16,21 +16,21 @@ public class AccountCreation
 	 System.out.println("Enter you 10 digit Mobile Number:");
 	long mnum=sc.nextLong();
 	System.out.println("Enter you 12 digit Adhar Number:");
-	long adharnum=sc.nextLong();
+	long adhaarnum=sc.nextLong();
 	long accnum=(long)(Math.random()*100000000000000L+1);
 	String qry="insert into Account values(?,?,?,?,?)";
 	PreparedStatement ps=con.prepareStatement(qry);
 	ps.setLong(1,accnum);
 	ps.setString(2, fname+lname);
 	ps.setLong(3, mnum);
-	ps.setLong(4, adharnum);
+	ps.setLong(4, adhaarnum);
 	ps.setLong(5, 0);
 	int rs=ps.executeUpdate();
 	if(rs==1)
 	{
 		System.out.println("Account Added Succecsfully");
 		System.out.println("Your Account Number is:"+accnum);
-		System.out.println("please Note your Account number for Futur uses!!!");
+		System.out.println("please Note your Account number for Future uses!!!");
 		
 	}
 	}
