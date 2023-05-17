b260b1e9178e146f3914497c75d2863509a465d8
Pavansomisetty32 <110320361+Pavansomisetty32@users.noreply.github.com>
2023-05-17 07:44:01 +0530

    Deposit.java

--- src/Deposit.java
+++ src/Deposit.java
@@ -2,9 +2,9 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class Deposite
+public class Deposit
 {
-	Deposite(Connection con,long anum,Scanner input)throws Exception
+	Deposit(Connection con,long anum,Scanner input)throws Exception
 	{
 		System.out.println("Enter Amount to Deposit:");
 		long amount=input.nextLong();
@@ -12,12 +12,12 @@ public class Deposite
 		{
 		long bal=Balance.balance(con, anum);
 		amount+=bal;
-		String qry="update Account set balence="+amount+" where accnum="+anum;
+		String qry="update Account set balance="+amount+" where accnum="+anum;
 		Statement st=con.createStatement();
 		int res=st.executeUpdate(qry);
 		if(res==1)
 		{
-			System.out.println("Amount Deposited SuccesFully");
+			System.out.println("Amount Deposited Successfully");
 		}
 		}
 		else
@@ -27,9 +27,9 @@ public class Deposite
 			
 		
 	}
-	Deposite(Connection con,long anum,long amount)throws Exception
+	Deposit(Connection con,long anum,long amount)throws Exception
 	{
-		String qry="update Account set balence="+amount+" where accnum="+anum;
+		String qry="update Account set balance="+amount+" where accnum="+anum;
 		Statement st=con.createStatement();
 		st.executeUpdate(qry);
 	
